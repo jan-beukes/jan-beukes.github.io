f4e64888867d0e24a4223b45a84cb9b4eb90bc77
Jan Beukes <beukes.janadam@gmail.com>
2025-08-11 18:11:44 +0200

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,5 +3,3 @@ sshfs 28087887@bach.sun.ac.za:/home/28087887 ~/nfs-home
 ~/nfs-home/cs-stow.sh ~/nfs-home --backup
 ln -s ~/nfs-home/Projects ~/
 ln -s ~/nfs-home/Software ~/
-
-setxkbmap -option caps:swapescape # maybe remap caps?
